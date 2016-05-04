4740a7aa37aa462a545f8d7d8b306f75f632c4ba
Ian Adrian <100043199@student.swin.edu.au>
2016-05-04 13:38:39 +1000

    added Utilities class
    Added the utilities header and it's method

---
+++ iEngine/iEngine/Utilities.cpp
@@ -0,0 +1,26 @@
+#include "Utilities.h"
+
+
+Utilities::Utilities()
+{
+}
+
+
+
+Utilities::~Utilities()
+{
+}
+
+Predicate Utilities::spliceString(char aString[])
+{
+	char* lDelimiter;
+	lDelimiter = strtok(aString, ";");
+
+	Predicate result[];
+	while (lDelimiter != NULL)
+	{
+		result[]
+	}
+
+	return result;
+}
