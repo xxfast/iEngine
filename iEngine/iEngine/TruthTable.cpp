30dd43bde5886957dfef81375fd04ad0130daf36
xxfast <isuru@xfastgames.com>
2016-05-12 18:13:16 +1000

    Added the initial truth table class

---
+++ iEngine/iEngine/TruthTable.cpp
@@ -0,0 +1,9 @@
+//
+//  TruthTable.cpp
+//  iEngine
+//
+//  Created by Isuru Kusumal Rajapakse on 5/12/16.
+//  Copyright © 2016 Isuru Kusumal Rajapakse. All rights reserved.
+//
+
+#include "TruthTable.h"
---
+++ iEngine/iEngine/TruthTable.h
@@ -0,0 +1,20 @@
+//
+//  TruthTable.hpp
+//  iEngine
+//
+//  Created by Isuru Kusumal Rajapakse on 5/12/16.
+//  Copyright © 2016 Isuru Kusumal Rajapakse. All rights reserved.
+//
+
+#ifndef TruthTable_hpp
+#define TruthTable_hpp
+
+#include <stdio.h>
+
+class TruthTable {
+    
+public:
+    
+};
+
+#endif /* TruthTable_hpp */
