dbee061252dea55b3784584a0c291c2552d042de
xxfast <isuru@xfastgames.com>
2016-05-12 21:03:48 +1000

    Removed the framework
    freaking wasted hours on using gtest, now i’m implementing my own
    target test program

---
+++ iEngine/IEngineTest/main.cpp
@@ -0,0 +1,25 @@
+//
+//  main.cpp
+//  IEngineTest
+//
+//  Created by Isuru Kusumal Rajapakse on 5/12/16.
+//  Copyright © 2016 Isuru Kusumal Rajapakse. All rights reserved.
+//
+
+#include <iostream>
+#include "Predicate.h"
+#include "TruthTable.h"
+
+using namespace std;
+
+void TestTruthTable()
+{
+    
+}
+
+int main(int argc, const char * argv[])
+{
+    cout << "Testing IEngine" <<endl;
+    TestTruthTable();
+    return 0;
+}
