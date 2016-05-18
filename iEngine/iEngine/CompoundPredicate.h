5c7944344604e9eef86c9f724062f898b549cdaa
SRISAIYEEGHARAN <100680347@student.swin.edu.au>
2016-05-18 21:56:01 +1000

    added predicate class

--- iEngine/iEngine/CompoundPredicate.h
+++ iEngine/iEngine/CompoundPredicate.h
@@ -4,10 +4,13 @@ class CompoundPredicate
 {
 private:
 	// MARK: Members
-
+	Predicate fLVal; //left side Predicate
+	Predicate fRVal; //right side Predicate
+	Connective fConnective; //operation between the value
 public:
 	// MARK: Constructors
-	CompoundPredicate();
+	CompoundPredicate(Predicate aPredicateOne, Predicate aPredicateTwo, );
+
 	~CompoundPredicate();
 
 	// MARK: Member functions
