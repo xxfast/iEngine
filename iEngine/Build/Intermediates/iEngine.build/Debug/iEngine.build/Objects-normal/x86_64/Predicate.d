1d00b188c8afd150fe5e7663fc6cea9aeb448cc3
xxfast <isuru@xfastgames.com>
2016-05-23 20:25:17 +1000

    added some changes to the main program

--- iEngine/Build/Intermediates/iEngine.build/Debug/iEngine.build/Objects-normal/x86_64/Predicate.d
+++ iEngine/Build/Intermediates/iEngine.build/Debug/iEngine.build/Objects-normal/x86_64/Predicate.d
@@ -2,4 +2,5 @@ dependencies: \
   /Users/Isuru/Developer/Git/iEngine/iEngine/iEngine/Predicate.cpp \
   /Users/Isuru/Developer/Git/iEngine/iEngine/iEngine/Predicate.h \
   /Users/Isuru/Developer/Git/iEngine/iEngine/iEngine/Utilities.h \
-  /Users/Isuru/Developer/Git/iEngine/iEngine/iEngine/IEngine.h
+  /Users/Isuru/Developer/Git/iEngine/iEngine/iEngine/IEngine.h \
+  /Users/Isuru/Developer/Git/iEngine/iEngine/iEngine/TruthTable.h
