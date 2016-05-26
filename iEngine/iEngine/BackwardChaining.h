eed2b7b734287c60d9a42ca7663974e792841df7
SRISAIYEEGHARAN <100680347@student.swin.edu.au>
2016-05-26 12:40:28 +1000

    added backward chain class

---
+++ iEngine/iEngine/BackwardChaining.h
@@ -0,0 +1,15 @@
+//  BackwardChaining.h
+//  iEngine
+//
+//  Created by Srisaiyeegharan Kidnapillai on 5/4/16.
+//  Copyright © 2016 Isuru Kusumal Rajapakse. All rights reserved.
+//
+
+#pragma once
+class BackwardChaining
+{
+public:
+	BackwardChaining();
+	~BackwardChaining();
+};
+
