faeb3ea329f6933912e7b1a2d6c0802db9be89ec
Ian Adrian <100043199@student.swin.edu.au>
2016-05-26 20:13:01 +1000

    fixed some method

--- iEngine/iEngine/BackwardChaining.h
+++ iEngine/iEngine/BackwardChaining.h
@@ -1,15 +1,28 @@
-//  BackwardChaining.h
-//  iEngine
-//
-//  Created by Srisaiyeegharan Kidnapillai on 5/4/16.
-//  Copyright © 2016 Isuru Kusumal Rajapakse. All rights reserved.
-//
+#include "Utilities.h"
+#include "Predicate.h"
+#include "IEngine.h"
+
 
 #pragma once
+struct HornRecord
+{
+	Predicate* fPredicate;
+	int fCount;
+};
+
 class BackwardChaining
 {
+private:
+	bool fStatus;
+	vector<Variable> fAgenda;
+	vector<Variable> fInferred;
+	vector<HornRecord> fHorns;
+
+
 public:
 	BackwardChaining();
+	BackwardChaining(vector<Predicate*> aPredicates);
 	~BackwardChaining();
+	vector<Variable> evaluate(Variable aVariableAsked);
 };
 
